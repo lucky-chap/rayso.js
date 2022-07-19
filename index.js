e14f3e8744a77560ff20afd4c3d9b48ac355cf6c
Quavo <hunchodotdev@gmail.com>
2022-07-19 15:45:40 +0000

    chore: :fire: finish class logic

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * Unofficial API for Ray.So that turns your code into beautiful images.
+ * Choose from a range of syntax colors, hide or show the background, and
+ * toggle between a dark and light window.
+ *
+ * Source: ray.so
+ * Author: Quavo https://github.com/lucky-chap with help from s0ftik3
+ */
+
+module.exports = require('./src/rayso');
